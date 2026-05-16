67624619b38ba30619bf17c4aa6452b4e9e9cf21
Dipanshu Raj <rajdipanshu180@gmail.com>
2026-05-17 00:29:05 +0530

    useChatStore and chatpa ge setup

---
+++ frontend/src/components/ContactLists.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactLists() {
+  return (
+    <div>ContactLists</div>
+  )
+}
+
+export default ContactLists
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore.js'
+import { useChatStore } from '../store/useChatStore.js'
+import BorderAnimatedComponent from '../components/BorderAnimatedComponent.jsx'
+import ProfileHeader from '../components/ProfileHeader.jsx'
+import ActiveTabSwitch from '../components/ActiveTabSwitch.jsx'
+import ChatsList from '../components/ChatsList.jsx'
+import ContactLists from '../components/ContactLists.jsx'
+import ChatContainer from '../components/ChatContainer.jsx'
+import NoConversationPlaceholder from '../components/NoConversationPlaceholder.jsx'
+
+
 
 function ChatPage() {
   const {logout} = useAuthStore()
+  const {activeTab,selectedUser} = useChatStore()
+
   return (
-    <div className='z-10'>
-      ChatPage
-      <button onClick={logout}>logout</button>
+    <div className='relative w-full max-w-6xl h-[800px]'>
+
+      <BorderAnimatedComponent>
+        {/* LEFT SIDE */}
+        <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
+          <ProfileHeader/>
+          <ActiveTabSwitch/>
+
+          <div className='flex-1 overflow-y-auto p-4 space-y-2'>
+            {activeTab === "chats" ? <ChatsList /> : <ContactLists/>}
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className='flex-1 flex-col bg-slate-900/50 backdrop-blur-sm'>
+        {selectedUser ? <ChatContainer/> : <NoConversationPlaceholder/>}
+        </div>
+      </BorderAnimatedComponent>
+  
     </div>
   )
 }
