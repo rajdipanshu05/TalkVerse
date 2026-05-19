4fc4e2b6e9f2423c1b66e7c122a50465a60a5fe4
Dipanshu Raj <rajdipanshu180@gmail.com>
2026-05-19 16:05:07 +0530

    finall

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-    baseURL: import.meta.env.MODE === "https://talkverse-c8lp.onrender.com/api"
+    baseURL: import.meta.env.MODE === "https://talkverse-c8lp.onrender.com/api",
     withCredentials: true
 })
