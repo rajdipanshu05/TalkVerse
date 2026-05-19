d9e67b1e1bd57a74651608108e51105cda17506c
Dipanshu Raj <rajdipanshu180@gmail.com>
2026-05-19 16:00:45 +0530

    final

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = ENV.PORT || 3000;
 
 //payload too large error
 app.use(express.json({limit:"5mb"})); //req.body
-app.use(cors({origin : "http://localhost:5173", credentials : true}))
+app.use(cors({origin : "", credentials : true}))
 app.use(cookieParser());
 
 
